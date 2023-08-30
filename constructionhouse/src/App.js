ede5bd05296874b06694b1b71a656b68a6029620
subham swain <subham.swain@targetintegration.com>
2023-08-30 11:26:56 +0530

    civilconstruction

---
+++ constructionhouse/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Construction from './components/construction'; // Correct the import
+
+function App() {
+  return (
+    <div className="App">
+      <Construction /> {/* Use PascalCase for components */}
+    </div>
+  );
+}
+
+export default App;
